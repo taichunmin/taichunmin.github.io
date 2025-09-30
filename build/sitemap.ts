f46614c4454624f1190f448986a0a47b6bd2035e
taichunmin <taichunmin@gmail.com>
2025-09-30 10:26:27 +0800

    sitemap

---
+++ build/sitemap.ts
@@ -0,0 +1,56 @@
+import { getSiteurl } from './dotenv'
+
+import * as _ from 'lodash-es'
+import { fileURLToPath } from 'url'
+import { promises as fsPromises } from 'fs'
+import dayjs from 'dayjs'
+import fg from 'fast-glob'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const distDir = path.resolve(__dirname, '../dist/')
+
+function toUrl (url: string): string {
+  url = url.replace(/[/]index[.]html$/, '/')
+  return `<url><loc>${url}</loc><changefreq>daily</changefreq></url>`
+}
+
+function toUrlset (urls: string[]): string {
+  return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${_.join(_.map(urls, toUrl), '')}</urlset>`
+}
+
+function toSitemap ({ lastmod, url }: { lastmod: string, url: string }): string {
+  return `<sitemap><loc>${url}</loc><lastmod>${lastmod}</lastmod></sitemap>`
+}
+
+function toSitemapIndex ({ lastmod, urls }: { lastmod: string, urls: string[] }): string {
+  return `<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${_.join(_.map(urls, url => toSitemap({ lastmod, url })), '')}</sitemapindex>`
+}
+
+async function writeSitemapByUrls ({ baseurl, dist, urls }: GenSitemapArgs): Promise<void> {
+  const sitemapIndex: string[] = []
+  const lastmod = dayjs().format('YYYY-MM-DDTHH:mmZ')
+  for (const [index, chunk] of _.toPairs(_.chunk(urls, 1000))) {
+    await fsPromises.writeFile(path.join(dist, `sitemap_${index}.xml`), toUrlset(chunk))
+    sitemapIndex.push(new URL(`sitemap_${index}.xml`, baseurl).href)
+  }
+  await fsPromises.writeFile(path.join(dist, 'sitemap.xml'), toSitemapIndex({ lastmod, urls: sitemapIndex }))
+}
+
+interface GenSitemapArgs {
+  baseurl: string
+  dist: string
+  urls: string[]
+}
+
+export async function build (): Promise<void> {
+  const urls = _.chain(await fg('**/*.html', { cwd: distDir }))
+    .map(getSiteurl)
+    .value()
+  await writeSitemapByUrls({ baseurl: getSiteurl(), dist: distDir, urls })
+}
+
+build().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
